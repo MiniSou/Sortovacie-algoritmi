a39f4d086bc81f4fca1cbdbd4ae59fa368747d20
MiniSou <91061465+MiniSou@users.noreply.github.com>
2022-03-21 13:13:22 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import random
+import time
+import sorts
+
+def r_list(length, max_cislo):
+    t_list = list()
+    for i in range(length):
+        t_list.append(random.randint(0, max_cislo))
+    return t_list
+
+def test():
+    uo_list = r_list(5000, 500)
+    start = time.time()
+    sorts.bubble_sort(list(uo_list))
+    end = time.time()
+    print("BubbleSort:", (end - start), "sec")
+
+    start = time.time()
+    sorts.selection_sort(list(uo_list))
+    end = time.time()
+    print("SelectionSort:", (end - start), "sec")
+
+    start = time.time()
+    sorts.insertion_sort(list(uo_list))
+    end = time.time()
+    print("InsertionSort:", (end - start), "sec")
+
+    start = time.time()
+    sorts.quick_sort(list(uo_list))
+    end = time.time()
+    print("QuickSort:", (end - start), "sec")
+
+    start = time.time()
+    sorts.merge_sort(list(uo_list))
+    end = time.time()
+    print("MergeSort:", (end - start), "sec")
+
+    start = time.time()
+    sort_list = list(uo_list)
+    sort_list.sort()
+    end = time.time()
+    print("PythonSort:", (end - start), "sec")
+
+test()
